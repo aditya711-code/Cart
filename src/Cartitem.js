fa9c3e70595bade7852dc2f12ae099626d7278fe
Aditya Mane <adityamane711@gmail.com>
2022-11-13 00:16:30 +0530

    Added increase quantity

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -8,6 +8,12 @@ class Cartitem extends  React.Component{
             qty:1,
             img:''
         }
+        //this.increaseQuantity=this.increaseQuantity.bind(this);
+    }
+    increaseQuantity=()=>{
+       
+        this.state.qty+=1;
+        console.log('this',this.state.qty);
     }
     render(){
         const{prices,title,qty}=this.state;
@@ -20,12 +26,24 @@ class Cartitem extends  React.Component{
                 <div className="right-block">
                     <div style={{fontSize:30}}>{title}</div>
                     <div style={{color:'grey'}}>Rs{prices}</div>
-                    <div style={{color:'#777'}}>Qty:{qty}</div>
+                    <div style={{color:'#777'}}>Qty:{this.state.qty}</div>
                     <div className="cart-item-actions">
                         {}
-                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"/>
-                        <img alt="decrease" className="action-icons" src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"/>
-                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png"/>
+                        <img alt="increase" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                            onClick={this.increaseQuantity}
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png"
+                        />
                     </div>
 
                 </div>
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+ <img alt="increase" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+                        onClick={this.increase}/>
+                        <img 
+                        alt="decrease" 
+                        className="action-icons" 
+                        src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"
+                        />
+                        <img 
+                        alt="delete" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png"
+                        />
